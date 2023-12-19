584c2b927d5422261a98db71304373933dea2b71
Sblohar <surabhilohar2000@gmail.com>
2023-12-19 19:25:39 +0530

    [ADD] Added solution for task 4

---
+++ solution/website_management/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': "Website Management",
+    'summary': "",
+    'description': "",
+
+    'author': "",
+    'website': "",
+    'sequence': 100,
+
+    'category': "Extra",
+    'version': "1.0",
+    'depends': ['base', 'web', 'website'],
+    'data': [
+           # 'security/ir.model.access.csv',
+           #  'views/custom_modul_template.xml',
+           #  'views/custom_page.xml',
+           #  'views/menus.xml',
+            'views/website_homepage.xml',
+    ],
+    'application': 'true',
+    'assets': {
+
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+
+}
