04d318de9f8f9c7f5a72e42f3e429ddb322a62e3
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-02 10:52:30 +0530

    Update script.js

--- solution/website_management/static/src/js/script.js
+++ solution/website_management/static/src/js/script.js
@@ -1,18 +1,47 @@
-odoo.define("website_management.register", function (require) {
-    "use strict";
+odoo.define('website_rdc.Register_form', function (require) {
+    'use strict';
+
+    const dom = require('web.dom');
     const publicWidget = require('web.public.widget');
+
 
     publicWidget.registry.register_form1 = publicWidget.Widget.extend({
         selector: '.register_form1',
         events: {
-            'change #country_id': '_onCountryChange',
-        },
-        init: function () {
-            this._super.apply(this, arguments);
-            this.updateSelection();
+            'change #country_id': '_onchangeCountry',
+            'change #state_id': '_onchangeState'
         },
 
-         onCountryChange(countryid) {
-      
-}
+
+        start: function () {
+            debugger
+        },
+
+          _onchangeCountry: function(ev) {
+               const country_id =  parseInt($(ev.currentTarget).val());
+               $('#state_id option').each(function(){
+                   var countryID = parseInt($(this).attr('country'));
+                   if(country_id !== countryID){
+                     $(this).addClass('d-none');
+                   }
+                   if(country_id === countryID){
+                     $(this).removeClass('d-none');
+                   }
+
+               })
+
+          },
+
+        _onchangeState: function(ev) {
+        debugger
+            var stateID = $(ev.currentTarget).val();
+            var Country = $(ev.currentTarget).find(":selected").attr('country');
+            if (Country)
+                $('#country_id').val(Country);
+        },
+
+
+    });
+
+
 });
