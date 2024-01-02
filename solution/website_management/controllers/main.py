ea2c24d9146fdc19140977240ff27f76261d0f79
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-02 19:36:56 +0530

    Update main.py

--- solution/website_management/controllers/main.py
+++ solution/website_management/controllers/main.py
@@ -7,16 +7,18 @@ class CustomController(http.Controller):
     @http.route(['/register_user'], type='http', auth='public', website=True)
     def render_custom_user_(self, **kw):
         domain = []
+
         state = request.env['res.country.state'].sudo().search(domain)
         countries = request.env['res.country'].sudo().search(domain)
+        partners = request.env['res.partner'].sudo().search(domain)
+        email_list = []
+        for partner in partners:
+            email_list.append(partner.email)
+
         vals = {
-                'states': state,
-                'country': countries,
-                }
+            'states': state,
+            'country': countries,
+            'email': email_list,
+        }
 
         return http.request.render('website_management.test_homepage', vals)
-
-
-
-
-
