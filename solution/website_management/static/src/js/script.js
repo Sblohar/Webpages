162069429da83381d5564ae529f8d38ec5e67033
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-05 19:22:04 +0530

    Update script.js

--- solution/website_management/static/src/js/script.js
+++ solution/website_management/static/src/js/script.js
@@ -11,6 +11,8 @@ odoo.define('website_rdc.Register_form', function (require) {
             'change #country_id': '_onchangeCountry',
             'change #state_id': '_onchangeState',
             'focusout #email_id': '_validemail',
+            'focusout #password_id': '_validatePassword',
+            'keyup #confirmPassword': '_matchPasswords'
 
         },
         start: function () {
@@ -41,31 +43,62 @@ odoo.define('website_rdc.Register_form', function (require) {
         },
 
 
-        _validemail: function (ev) {
+          _validemail: function (ev) {
             debugger
-           const enteredEmail = $('.email_list').val();
-           const emailList = $('.email_list');
+              const enteredEmail = $('.email_list').val();
+                const emailList = [];
 
-//           let emailExists = $('.email_list').val();
-           let emailExists = false;
+                let emailExists = $('.email_list').val().includes(ev.currentTarget.value);
 
-            emailList.each(function () {
-            if ($(this).text() === enteredEmail) {
-            emailExists = true;
-            return false;
+                if (emailExists) {
+                         document.getElementById("email-warning-already-exist").classList.remove("d-none");
+                         document.getElementById("email-already").classList.add("d-none");
+
+                 } else {
+                        document.getElementById("email-already").classList.remove("d-none");
+                        document.getElementById("email-warning-already-exist").classList.add("d-none");
+                 }
+            },
+
+           _validatePassword: function (ev) {
+             const password = ev.currentTarget.value;
+
+                const passwordPattern = /^(?=.*\d)(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?=.*[A-Z])(?=.*[a-z]).{8,}$/;
+
+                if (passwordPattern.test(password)) {
+                      document.getElementById('passwordError').style.color = 'green';
+                      document.getElementById('passwordError').innerHTML
+                                    = 'Password is valid';
+                } else {
+                     document.getElementById('passwordError').style.color = 'red';
+                     document.getElementById('passwordError').innerHTML
+                                    = 'Please enter a valid password';
+                }
+
+           },
+
+            _matchPasswords: function (ev) {
+
+                const confirmPassword = ev.currentTarget.value;
+                const password = document.getElementById('password_id').value;
+
+                if (password !== confirmPassword) {
+                    document.getElementById('confirmPasswordMessage').style.color = 'red';
+                    document.getElementById('confirmPasswordMessage').innerHTML
+                            = 'Use same password';
+
+                } else {
+                        document.getElementById('confirmPasswordMessage').style.color = 'green';
+                        document.getElementById('confirmPasswordMessage').innerHTML =
+                         'Password Matched';
+
+                }
             }
-        });
-
-            if (emailExists) {
-                alert('Email already exists!');
-            } else  {
-                alert('Email does not exist!');
-            }
-        },
-
 
     });
 
 
 });
+
+
 
