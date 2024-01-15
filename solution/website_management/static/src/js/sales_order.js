903eaa3b84d2c740cb999543266468dc230f0890
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-15 18:55:29 +0530

    Update sales_order.js

--- solution/website_management/static/src/js/sales_order.js
+++ solution/website_management/static/src/js/sales_order.js
@@ -7,6 +7,10 @@ odoo.define('website_rdc.Sales_order', function (require) {
         selector: '.sale_order1',
         events: {
             'input #internalReferenceSearch': '_searchReference',
+            'click .btn-danger': '_addToCart',
+            'click #confirm-btn': '_confirmOrder',
+            'click .remove-btn': '_removeCart',
+
         },
 
         start: function () {
@@ -14,13 +18,54 @@ odoo.define('website_rdc.Sales_order', function (require) {
         },
 
         _searchReference: function (ev) {
-            const searchValue = ev.target.value.toLowerCase();
+           const searchValue = ev.target.value.toLowerCase();
 
-            this.$('.main1').each(function () {
-                const internalReference = $(this).find('.name').text().toLowerCase();
-                const internal = internalReference.includes(searchValue);
-                $(this).toggle(internal);
+           this.$('.main1').each(function () {
+           const internalReference = $(this).find('.name').text().toLowerCase();
+           const internal = internalReference.includes(searchValue);
+
+            const nameSearch = $(this).find('.description').text().toLowerCase();
+            const nameFound = nameSearch.includes(searchValue);
+
+            $(this).toggle(internal || nameFound);
+           });
+        },
+
+        _addToCart: function (ev) {
+            const $row = $(ev.currentTarget).closest('.main1');
+            const internalReference = $row.find('.name').text();
+            const description = $row.find('.description').text();
+            const price = $row.find('.price').text();
+            const quantity = $row.find('.quantity input').val();
+            const imagebox = $row.find('.image img').attr('src');
+
+            const newRow = `<tr>
+                                <td></td>
+                                 <td><img src="${imagebox}" alt="Image" style="height:80px;"/></td>
+                                <td>${internalReference}</td>
+                                <td>${description}</td>
+                                <td>${price}</td>
+                                <td>${quantity}</td>
+                                 <td>
+                                    <button class="btn btn-outline-danger m-3 border border-danger remove-btn">Remove</button>
+                                </td>
+                            </tr>`;
+
+            const $ordersTable = $('.orders table');
+            $ordersTable.append(newRow);
+
+            $ordersTable.find('tr').each(function (index) {
+                $(this).find('td:first').text(index + 0);
             });
+        },
+
+        _confirmOrder: function (ev){
+            alert("Order Confirmed !")
+
+        },
+        _removeCart: function (ev){
+            const $row = $(ev.currentTarget).closest('tr');
+            $row.remove();
         },
     });
 });
