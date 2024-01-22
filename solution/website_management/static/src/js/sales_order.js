a7cdbfab3b1243b1866dc07d5f02bc0e85d09eee
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-22 18:10:39 +0530

    Update sales_order.js

--- solution/website_management/static/src/js/sales_order.js
+++ solution/website_management/static/src/js/sales_order.js
@@ -10,7 +10,6 @@ odoo.define('website_rdc.Sales_order', function (require) {
             'click .btn-danger': '_addToCart',
             'click #confirm-btn': '_confirmOrder',
             'click .remove-btn': '_removeCart',
-
         },
 
         start: function () {
@@ -18,17 +17,17 @@ odoo.define('website_rdc.Sales_order', function (require) {
         },
 
         _searchReference: function (ev) {
-           const searchValue = ev.target.value.toLowerCase();
+            const searchValue = ev.target.value.toLowerCase();
 
-           this.$('.main1').each(function () {
-           const internalReference = $(this).find('.name').text().toLowerCase();
-           const internal = internalReference.includes(searchValue);
+            this.$('.main1').each(function () {
+                const internalReference = $(this).find('.name').text().toLowerCase();
+                const internal = internalReference.includes(searchValue);
 
-            const nameSearch = $(this).find('.description').text().toLowerCase();
-            const nameFound = nameSearch.includes(searchValue);
+                const nameSearch = $(this).find('.description').text().toLowerCase();
+                const nameFound = nameSearch.includes(searchValue);
 
-            $(this).toggle(internal || nameFound);
-           });
+                $(this).toggle(internal || nameFound);
+            });
         },
 
         _addToCart: function (ev) {
@@ -36,18 +35,17 @@ odoo.define('website_rdc.Sales_order', function (require) {
             const internalReference = $row.find('.name').text();
             const description = $row.find('.description').text();
             const price = $row.find('.price').text();
-            const quantity = $row.find('.quantity input').val();
+            const quantity = $row.find('.quantity input').val() || 0;
             const imagebox = $row.find('.image img').attr('src');
 
-           debugger
             const newRow = `<tr prod_id=${$row.attr('prod_id')}>
                                 <td></td>
-                                 <td><img src="${imagebox}" alt="Image" style="height:80px;"/></td>
+                                <td><img src="${imagebox}" alt="Image" style="height:80px;"/></td>
                                 <td>${internalReference}</td>
                                 <td>${description}</td>
                                 <td>${price}</td>
-                                <td>${quantity}</td>
-                                 <td>
+                                <td><input type="number" style="width: 60px;" value="${quantity}"/></td>
+                                <td>
                                     <button class="btn btn-outline-danger m-3 border border-danger remove-btn">Remove</button>
                                 </td>
                             </tr>`;
@@ -60,73 +58,45 @@ odoo.define('website_rdc.Sales_order', function (require) {
             });
         },
 
-         _removeCart: function (ev){
+        _removeCart: function (ev) {
             const $row = $(ev.currentTarget).closest('tr');
             $row.remove();
         },
 
-
-        _confirmOrder: function (){
+        _confirmOrder: function () {
             let orders = this._confirm_order_data();
-            debugger
             $.ajax("/confirm_order", {
                 data: orders,
                 type: 'POST',
                 dataType: 'json',
                 success: function (data) {
-                         debugger
+                    alert("helooo")
                 },
                 error: function (error) {
-                debugger
+                    debugger;
                 }
-            })
-          },
+            });
+        },
 
-//           _confirm_order_data: function () {
-//               let order_data = {};
-////                order_data['customer_id'] = $('#customer_id').val();
-////                order_data['product_id'] = $('#product_id')[0].innerText;
-////                order_data['quantity'] = $('#quantity')[0].lastElementChild.value;
-//
-//                order_data['customer_id'] = $('#customer_id').val();
-//                order_data['orders_line'] = [];
-//               var $element = $('.order_sale_table').find('tbody tr');
-//
-//             $element.each(function (index) {
-//                var $el = $($('.orders_col')[index]);
-//
-//                    let product_id = $el.data('prod_id');
-//                    let quantity = $(this).find('#quantity input').val();
-//
-//                    order_data['orders_line'].push({
-//                        'product_id': product_id,
-//                        'quantity': quantity
-//                    });
-//                });
-//                      return order_data;
-//
-//            },
+        _confirm_order_data: function () {
+            debugger;
 
-            _confirm_order_data: function () {
-                let order_data = {};
-                order_data['customer_id'] = $('#customer_id').val();
-                order_data['order_line'] = [];
+            let order_data = {};
+            order_data['customer_id'] = $('#customer_id').val();
+            order_data['order_line'] = [];
 
-                 $('.order_sale_table tbody tr').each(function (index) {
-                     let product_id = $(this).attr('prod_id');
-                     let quantity = $('quantity').val();
+            $('.orders table tbody tr').each(function (index) {
+                debugger;
+                let product_id = $(this).attr('prod_id');
+                let quantity = $(this).find('td:nth-child(6) input').val() || 0;
 
-                    order_data['order_line'].push({
-                        'product_id': product_id,
-                        'product_uom_qty': quantity
-                    });
+                order_data['order_line'].push({
+                    'product_id': product_id,
+                    'product_uom_qty': quantity
                 });
+            });
 
-
-    return order_data;
-},
-
-
-
-        });
+            return order_data;
+        },
+    });
 });
