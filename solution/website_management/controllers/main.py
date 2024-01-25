23073bcf07466b6a6c4033a8cb380adcf1da1e82
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-01-25 12:19:39 +0530

    Update main.py

--- solution/website_management/controllers/main.py
+++ solution/website_management/controllers/main.py
@@ -52,45 +52,9 @@ class CustomController(http.Controller):
 
         return request.redirect('/website_management/contact_info/%s' % partner_id.id)
 
-    # @http.route(['/confirm_order'], type='http', auth='public', website=True)
-    # def create_confirm_order(self, **post):
-    #     print("tttttttttttttttttttttttttt")
-    #
-    #     customer_id = post.get('customer_id')
-    #     quantity = int(post.get('quantity', 0))
-    #     product_id = int(post.get('product_id', 0))
-    #
-    #     print(post)
-    #
-    #     sale_order = request.env['sale.order'].sudo().create({
-    #         'partner_id': int(customer_id),
-    #         'order_line': [
-    #             Command.create({
-    #                 'product_id': product_id,
-    #                 'product_uom_qty': quantity,
-    #            })
-    #
-    #         ]
-    #
-    #     })
-    #
-    #     return request.redirect('/website_management/order_new_sales/%s' % sale_order.id)
-    #
-    #     # return json.dumps({
-    #     #     'status': 200
-    #     # })
     @http.route(['/new_confirm_order'], type='http', auth='public', methods=['POST'], website=True,  csrf=False)
     def create_confirm_order(self, **kw):
-
         customer_id = kw.get('customer_id')
-        # # order_lines = kw.get('order_line', [])
-        #
-        # print(kw)
-        #
-        vals = {
-            'partner_id': int(customer_id),
-        }
-
         data = kw
 
         order_lines = {}
@@ -108,24 +72,10 @@ class CustomController(http.Controller):
 
         print('order_data', order_data)
 
-        order_lines = []
         product_id = int(order_data.get('product_id'))
         product_uom_qty = int(order_data.get('product_uom_qty', 0))
 
-        # order_line = {
-        #     'product_id': product_id,
-        #     'product_uom_qty': product_uom_qty,
-        # }
-        #
-        # order_lines.append(order_line)
-
-        # vals = {
-            # 'order_line': order_line,
-        # }
-
-
         sale_order = request.env['sale.order'].sudo().create({
-            # 'partner_id': int(customer_id),
             'partner_id': int(customer_id),
             'order_line': [
                 Command.create({
@@ -135,9 +85,7 @@ class CustomController(http.Controller):
             ]
         })
 
-        # return request.redirect('/website_management/order_new_sales/%s' % sale_order.id)
         return json.dumps({'sale_order': sale_order.id})
-        # return request.redirect('/'% sale_order.id)
 
 
 
