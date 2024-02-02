22a5838b1a5ffd1727cdc603f92e019fb20c983b
Sblohar <89372351+Sblohar@users.noreply.github.com>
2024-02-02 18:49:21 +0530

    Update script.js

--- solution/website_management/static/src/js/script.js
+++ solution/website_management/static/src/js/script.js
@@ -1,132 +1,202 @@
-odoo.define('website_rdc.Register_form', function (require) {
+odoo.define('website_rdc.Sales_order', function (require) {
     'use strict';
 
-    const dom = require('web.dom');
     const publicWidget = require('web.public.widget');
+    const Dialog = require('web.Dialog');
 
-
-    publicWidget.registry.register_form1 = publicWidget.Widget.extend({
-        selector: '.register_form1',
+    publicWidget.registry.sale_order1 = publicWidget.Widget.extend({
+        selector: '.sale_order1',
         events: {
-            'change #country_id': '_onchangeCountry',
-            'change #state_id': '_onchangeState',
-            'focusout #email_id': '_validemail',
-            'focusout #password_id': '_validatePassword',
-            'keyup #confirmPassword': '_matchPasswords',
-            'click .create_user_btn-test': '_onclick_create_user'
-
+            'input #internalReferenceSearch': '_searchReference',
+            'click .btn-danger': '_addToCart',
+            'click #confirm-btn': '_confirmOrder',
+            'click .remove-btn': '_removeCart',
+            'click #create_customer': '_createCustomer'
         },
 
         start: function () {
-            debugger
-            console.log("TEST")
+             this.$('#confirm-btn').prop('disabled', true);
         },
 
-          _onchangeCountry: function(ev) {
+        _searchReference: function (ev) {
+            const searchValue = ev.target.value.toLowerCase();
 
-          debugger
-               const country_id =  parseInt($(ev.currentTarget).val());
-               $('#state_id option').each(function(){
-                   var countryID = parseInt($(this).attr('country'));
-                   if(country_id !== countryID){
-                     $(this).addClass('d-none');
-                   }
-                   if(country_id === countryID){
-                     $(this).removeClass('d-none');
-                   }
+            this.$('.main1').each(function () {
+                const internalReference = $(this).find('.name').text().toLowerCase();
+                const internal = internalReference.includes(searchValue);
 
-               })
+                const nameSearch = $(this).find('.description').text().toLowerCase();
+                const nameFound = nameSearch.includes(searchValue);
 
-          },
+                $(this).toggle(internal || nameFound);
+            });
+        },
 
-        _onchangeState: function(ev) {
-            var stateID = $(ev.currentTarget).val();
-            var Country = $(ev.currentTarget).find(":selected").attr('country');
-            if (Country)
-                $('#country_id').val(Country);
+        _addToCart: function(ev) {
+            const $row = $(ev.currentTarget).closest('.main1');
+            const internalReference = $row.find('.name').text();
+            const price = $row.find('.price').text();
+            const quantityInput = $row.find('.quantity input');
+            const quantity = parseInt(quantityInput.val()) || 0;
+            const $ordersTable = $('.order_sale_table');
+
+            quantityInput.removeClass('error-border');
+
+            const $alreadyStore = $ordersTable.find(`tr[prod_id="${$row.attr('prod_id')}"]`);
+
+            if ($alreadyStore.length > 0) {
+                const CurrentQ = parseInt($alreadyStore.find('td input').val()) || 0;
+                const NewQ = CurrentQ + quantity;
+                $alreadyStore.find('td input').val(NewQ);
+            } else {
+                if (quantity > 0) {
+                    const newRow = `<tr prod_id=${$row.attr('prod_id')}>
+                                        <td></td>
+                                        <td>${internalReference}</td>
+                                        <td>${price}</td>
+                                        <td><input type="number" style="width: 60px;" value="${quantity}" required/></td>
+                                        <td>
+                                            <button class="btn btn-outline-danger border border-danger remove-btn">Remove</button>
+                                        </td>
+                                    </tr>`;
+
+                    $ordersTable.append(newRow);
+
+                    $ordersTable.find('tr').each(function(index) {
+                        $(this).find('td:first').text(index + 0);
+                    });
+                } else {
+                    quantityInput.addClass('error-border');
+                }
+
+                if (this.$('.order_sale_table tbody tr').length > 0) {
+                    this.$('#confirm-btn').prop('disabled', false);
+                }
+            }
+               quantityInput.val('');
         },
 
 
-          _validemail: function (ev) {
-              const enteredEmail = $('.email_list').val();
-                const emailList = [];
+        _removeCart: function (ev) {
+            const $row = $(ev.currentTarget).closest('tr');
+            $row.remove();
+        },
 
-                let emailExists = $('.email_list').val().includes(ev.currentTarget.value);
+        _confirmOrder: function () {
 
-                if (emailExists) {
-                         document.getElementById("email-warning-already-exist").classList.remove("d-none");
-                         document.getElementById("email-already").classList.add("d-none");
-
-                 } else {
-                        document.getElementById("email-already").classList.remove("d-none");
-                        document.getElementById("email-warning-already-exist").classList.add("d-none");
-                 }
-            },
-
-           _validatePassword: function (ev) {
-             const password = ev.currentTarget.value;
-
-                const passwordPattern = /^(?=.*\d)(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?=.*[A-Z])(?=.*[a-z]).{8,}$/;
-
-                if (passwordPattern.test(password)) {
-                      document.getElementById('passwordError').style.color = 'green';
-                      document.getElementById('passwordError').innerHTML
-                                    = 'Password is valid';
-                } else {
-                     document.getElementById('passwordError').style.color = 'red';
-                     document.getElementById('passwordError').innerHTML
-                                    = 'Please enter a valid password';
+            $('#customer_id').change(function() {
+                $('#error_message').hide();
+            });
+             const customerId = $('#customer_id').val();
+                if (!customerId) {
+                      $('#error_message').text("Please select a customer.").show();
+                    return;
                 }
 
-           },
+            const order_data = this._confirm_order_data();
+                $.ajax("/new_confirm_order", {
+                    data: order_data,
+                    type: 'POST',
+                    dataType: 'json',
+                    success: function (data) {
+                        debugger;
+                        if(data){
+                            alert("Order confirmed successfully");
+                               $('#customer_id').val('');
+                               $('.order_sale_table tbody').empty();
 
-            _matchPasswords: function (ev) {
+                               $('#error_message').hide();
 
-                const confirmPassword = ev.currentTarget.value;
-                const password = document.getElementById('password_id').value;
 
-                if (password !== confirmPassword) {
-                    document.getElementById('confirmPasswordMessage').style.color = 'red';
-                    document.getElementById('confirmPasswordMessage').innerHTML
-                            = 'Use same password';
+                        }
+                    },
+                    error: function (error) {
+                           alert("Failed to confirm order. Please try again.");
+                    }
+                });
+        },
 
-                } else {
-                        document.getElementById('confirmPasswordMessage').style.color = 'green';
-                        document.getElementById('confirmPasswordMessage').innerHTML =
-                         'Password Matched';
+        _confirm_order_data: function () {
+            let order_data = {};
+            order_data['customer_id'] = $('#customer_id').val();
+            order_data['order_line'] = [];
 
-                }
-            },
+            $('.order_sale_table tbody tr').each(function (index) {
+                let product_id = $(this).attr('prod_id');
+                let quantity = $(this).find('td:nth-child(4) input').val() || 0;
+                order_data['order_line'].push({
+                    'product_id': product_id,
+                    'product_uom_qty': quantity,
+                });
+            });
 
-            _onclick_create_user: function (){
+            return order_data;
+        },
 
-                     let record = this._record_data_value();
-                        $.ajax("/create_record", {
-                                data: record,
-                                type: 'POST',
-                                dataType: 'json',
-                                success: function (data) {
+        _createCustomer: function () {
+            var self = this;
+            var $content = $('<div>').append(
+                 $('<label>', { class: 'input_label', text: 'First Name: ' }),
+                 $('<input>', { class: 'input_box1', id: 'first_name', type: 'text', name: 'first_name', placeholder: 'First Name' }),
+                 $('<br>'),
+                 $('<label>', { class: 'input_label mt-4', text: 'Last Name: ' }),
+                 $('<input>', { class: 'input_box1', id: 'last_name', type: 'text', name: 'last_name', placeholder: 'Last Name' })
+            );
 
-                                },
-                                error: function (error) {
-                                }
-                        })
-                  },
+            this.dialog = new Dialog(this, {
+                size:'medium',
+                title: 'Create New Customer',
+                buttons: [{
+                    text: 'Create',
+                    classes: 'btn-primary',
+                    close: true,
+                    click: function () {
+                        var firstName = $('#first_name').val();
+                        var lastName = $('#last_name').val();
 
-             _record_data_value: function () {
-                  debugger
-
-                      let record_data = {};
-                    record_data['first_name'] = $('#first_name').val();
-                    record_data['last_name'] = $('#last_name').val();
-
-                    return record_data;
+                         self._newCustomer(firstName, lastName);
+                    }
                 },
+                 {
+                    text: 'Cancel',
+                    close: true
+                }],
+                $content: $content,
+            });
 
-    })
+            this.dialog.open();
+        },
 
+        _newCustomer: function (firstName, lastName) {
+            var newCustomerId = firstName.toLowerCase() + '_' + lastName.toLowerCase();
 
+            $('#customer_id').append($('<option>', {
+                value: newCustomerId,
+                text: firstName + ' ' + lastName
+            }));
+            $('#customer_id').val(newCustomerId);
+
+                $.ajax({
+                    type: 'POST',
+                    url: '/create_record',
+                    data: {
+                        first_name: firstName,
+                        last_name: lastName,
+                        data_return: 'id',
+                        customer_id: parseInt(newCustomerId.replace('_', ''), 10)
+                    },
+                    success:function(response){
+                      if(response && typeof response === 'number'){
+
+                       }
+                    },
+                    error: function(error){
+
+                    }
+                });
+
+            return newCustomerId;
+
+        },
+    });
 });
-
-
-
